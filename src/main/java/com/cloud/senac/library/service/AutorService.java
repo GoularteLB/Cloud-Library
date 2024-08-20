cfd898e613e9eeb4c59d0786c9ff76a4c586cd13
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-08-20 20:48:27 -0300

    .

---
+++ src/main/java/com/cloud/senac/library/service/AutorService.java
@@ -0,0 +1,30 @@
+package com.cloud.senac.library.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AutorService implements IGenericService {
+
+    @Override
+    public Object Cadastrar(Object object) {
+        return null;
+    }
+
+    @Override
+    public Object Editar(Object object) {
+        return null;
+    }
+
+    @Override
+    public void Excluir(Object object) {
+
+    }
+
+    @Override
+    public Object Listar(Object object) {
+        return null;
+    }
+
+}
---
+++ src/test/java/com/cloud/senac/library/service/AutorServiceTest.java
@@ -0,0 +1,7 @@
+package com.cloud.senac.library.service;
+
+public class AutorServiceTest {
+
+    private AutorService autorService;
+
+}
