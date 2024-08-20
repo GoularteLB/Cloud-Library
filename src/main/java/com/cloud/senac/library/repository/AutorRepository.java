4b93a0282e8702b9f98346b504f04f988ab25f49
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-08-20 20:40:02 -0300

    feat: created new repository 'autorRepository'

---
+++ src/main/java/com/cloud/senac/library/repository/AutorRepository.java
@@ -0,0 +1,7 @@
+package com.cloud.senac.library.repository;
+
+import com.cloud.senac.library.entity.Autor;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AutorRepository extends JpaRepository<Autor, Long> {
+}
