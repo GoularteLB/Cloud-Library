0ee7e0348cefe3ca8c1d2048481939bbe38b2579
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-08-20 21:54:16 -0300

    fix: change Object type by T type in 'IGenericService'

--- src/main/java/com/cloud/senac/library/service/IGenericService.java
+++ src/main/java/com/cloud/senac/library/service/IGenericService.java
@@ -1,9 +1,12 @@
 package com.cloud.senac.library.service;
 
 
+import com.cloud.senac.library.dto.AutorDto;
+
 public interface IGenericService<T> {
-    T Cadastrar(Object object);
-    T Editar(Object object);
-    void Excluir(Object object);
-    T Listar(Object object);
+
+    T Cadastrar(T object);
+    T Editar(T object);
+    void Excluir(T object);
+    T Listar(T object);
 }
