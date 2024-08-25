455b5bd84d93e06aac711f75fe9efc5309ca2f1e
Luiz Antônio Mendes Nasário <luiznmendes2@gmail.com>
2024-08-25 02:14:03 -0300

    I created a user repository, a user mapper, and a user service.

--- src/main/java/com/cloud/senac/library/repository/UsuarioRepository.java
+++ src/main/java/com/cloud/senac/library/repository/UsuarioRepository.java
@@ -3,6 +3,14 @@ package com.cloud.senac.library.repository;
 import com.cloud.senac.library.entity.Autor;
 import com.cloud.senac.library.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    @Query("""
+        select a from Usuario a WHERE a.nome ilike :nome
+        """)
+    List<Usuario> findUsuarioByName(String nome);
 }
