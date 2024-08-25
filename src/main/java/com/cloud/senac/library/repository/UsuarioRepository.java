519e7d887adc2e806155425320deed950a67fd44
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-08-25 14:41:24 -0300

    feat: create new Query method to find user by email

--- src/main/java/com/cloud/senac/library/repository/UsuarioRepository.java
+++ src/main/java/com/cloud/senac/library/repository/UsuarioRepository.java
@@ -4,5 +4,10 @@ import com.cloud.senac.library.entity.Autor;
 import com.cloud.senac.library.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+
+    Optional<Usuario> findByEmail(String email);
+
 }
