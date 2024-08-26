b790dd378c3ce89b88ddac4b470f4ae2994601e3
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-08-26 20:16:30 -0300

    feat: implemented 'IAuthService' in 'AuthService'

--- src/main/java/com/cloud/senac/library/service/auth/AuthService.java
+++ src/main/java/com/cloud/senac/library/service/auth/AuthService.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
-public class AuthService {
+public class AuthService implements IAuthService<LoginDto, RegisterDto> {
 
     private final UsuarioRepository usuarioRepository;
     private final UsuarioService usuarioService;
