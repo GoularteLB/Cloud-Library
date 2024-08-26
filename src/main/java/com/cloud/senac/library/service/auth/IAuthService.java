e4aeaaaeaf8a600ae95d23cbb8ba838b583d6f06
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-08-26 20:16:09 -0300

    feat: created new interface 'IAuthService' to implentes in AuthService

--- src/main/java/com/cloud/senac/library/service/auth/IAuthService.java
+++ src/main/java/com/cloud/senac/library/service/auth/IAuthService.java
@@ -2,8 +2,10 @@ package com.cloud.senac.library.service.auth;
 
 import org.springframework.http.ResponseEntity;
 
-public interface IAuthService<T>{
+public interface IAuthService<L, R>{
 
-
+    ResponseEntity register(R dto);
+    ResponseEntity registerAdm(R dto);
+    ResponseEntity login(L dto);
 
 }
