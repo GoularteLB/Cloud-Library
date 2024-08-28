4656881e523a57699e71f37db15e33717c5edc29
corsalover <lucasozpre@gmail.com>
2024-08-28 00:06:24 -0300

    Revert "reserva's first commit"
    This reverts commit 5564ddb1bed8a35826a615705c6ae2feb415d6ec.

--- src/main/java/com/cloud/senac/library/dto/ReservaDto.java
+++
@@ -1,7 +0,0 @@
-package com.cloud.senac.library.dto;
-
-import java.time.LocalDate;
-
-public record ReservaDto (LocalDate dataInicial, LocalDate dataFinal, LocalDate dataEntrega, Double multa) {
-
-}
--- src/main/java/com/cloud/senac/library/repository/ReservaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.cloud.senac.library.repository;
-
-import com.cloud.senac.library.entity.Reserva;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ReservaRepository extends JpaRepository<Reserva, Long> {
-}
