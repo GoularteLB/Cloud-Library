eed071d9e580104fb56930ece51fd93482614068
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-08-29 21:17:53 -0300

    refact: remove all implementation in 'ReservaController'

---
+++ src/main/java/com/cloud/senac/library/controller/ReservaController.java
@@ -0,0 +1,12 @@
+package com.cloud.senac.library.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class ReservaController {
+
+
+
+}
