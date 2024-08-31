d47d2ec8d3fa9a521c3ea923a697cc41644095a2
Rafael-Floriano <rafael.florianoc9@gmail.com>
2024-08-30 22:00:50 -0300

    feat: change controller to consider admin permission

--- src/main/java/com/cloud/senac/library/controller/LivroController.java
+++ src/main/java/com/cloud/senac/library/controller/LivroController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/livrp")
+@RequestMapping("/admin/livrp")
 @RequiredArgsConstructor
 @Tag(name = "Livro Controller")
 public class LivroController {
